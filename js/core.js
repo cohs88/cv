33a89084bf33d0557a7b9be786cd92f666071c81
servando coria <cohs88@hotmail.com>
2013-12-02 19:58:44 -0800

    Se agrego scrollTo al proyecto

--- js/core.js
+++ js/core.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-	
+	$("#lbl_educacion").click(function(){
+		$.scrollTo("#educacion", 800);
+		return false;
+	});
+
+	$("#lbl_experiencia_laboral").click(function(){
+		$.scrollTo("#experiencia_laboral", 800);
+		return false;
+	});
 });
