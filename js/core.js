e0831aa1740830a66aa45494b90adab0f1b55e2d
servando coria <cohs88@hotmail.com>
2013-12-02 20:12:48 -0800

    Modulo proyectos personales

--- js/core.js
+++ js/core.js
@@ -8,4 +8,9 @@ $(document).ready(function(){
 		$.scrollTo("#experiencia_laboral", 800);
 		return false;
 	});
+
+	$("#lbl_proyectos_personales").click(function(){
+		$.scrollTo("#proyectos_personales", 800);
+		return false;
+	});
 });
